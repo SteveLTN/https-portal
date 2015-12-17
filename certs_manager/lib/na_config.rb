9d19477600d72698730ce333cb90496488a1a76a
Weiyan Shao <lighteningman@gmail.com>
2015-12-17 02:07:31 +0100

    Update prefix for env variables

--- certs_manager/lib/na_config.rb
+++ certs_manager/lib/na_config.rb
@@ -1,7 +1,7 @@
 module NAConfig
   def self.domains
-    ENV.select{ |k| k.start_with? 'FORWARD_' }.map do |k,v|
-      Domain.new(k.sub('FORWARD_', '').downcase, v)
+    ENV.select{ |k| k.start_with? 'NGINX_ACME_' }.map do |k,v|
+      Domain.new(k.sub('NGINX_ACME_', '').downcase, v)
     end
   end
 
