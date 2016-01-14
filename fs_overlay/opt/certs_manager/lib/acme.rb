cfc3aa393e3406de4099062f05f8ca132d079926
Weiyan Shao <lighteningman@gmail.com>
2016-01-14 22:01:37 +0100

    Gracefully handle ACME timeouts

--- fs_overlay/opt/certs_manager/lib/acme.rb
+++ fs_overlay/opt/certs_manager/lib/acme.rb
@@ -1,15 +1,23 @@
+require 'timeout'
+
 module ACME
   def self.sign(domain)
-    puts "Signing certificates from #{NAConfig.ca} ..."
+    Timeout::timeout(30) do
 
-    command = <<-EOC
-      acme_tiny \
-        --account-key /var/lib/https-portal/account.key \
-        --csr #{domain.csr_path} \
-        --acme-dir /var/www/challenges/ \
-        --ca #{NAConfig.ca} > #{domain.signed_cert_path}
-    EOC
+      puts "Signing certificates from #{NAConfig.ca} ..."
 
-    system(command)
+      command = <<-EOC
+        acme_tiny \
+          --account-key /var/lib/https-portal/account.key \
+          --csr #{domain.csr_path} \
+          --acme-dir /var/www/challenges/ \
+          --ca #{NAConfig.ca} > #{domain.signed_cert_path}
+      EOC
+
+      system(command)
+
+    end
+  rescue Timeout::Error => e
+    puts 'Signing certificates timed out. Is DNS set up properly?'
   end
 end
