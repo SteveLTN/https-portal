367e4d433bd20493d3638ea87d8cadb86edd92a5
Weiyan Shao <SteveLTN@users.noreply.github.com>
2018-04-27 19:59:53 +0200

    Use a dummy chain process for backward compatibility (#131)

--- fs_overlay/opt/certs_manager/lib/commands.rb
+++ fs_overlay/opt/certs_manager/lib/commands.rb
@@ -1,14 +1,9 @@
 require 'open-uri'
 
 module Commands
-  def download_intermediate_cert
-    unless File.exist? intermediate_cert_path
-      File.open(intermediate_cert_path, 'wb') do |saved_file|
-        open('https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem', 'rb') do |read_file|
-          saved_file.write(read_file.read)
-        end
-      end
-    end
+  def chain_cert(domain)
+    # Keeping this step for backward compatibility
+    system "ln -s #{domain.signed_cert_path} #{domain.chained_cert_path}"
   end
 
   def mkdir(domain)
@@ -21,9 +16,5 @@ module Commands
     File.open('/etc/hosts', 'a') do |f|
       f.puts "#{docker_host_ip}\tdockerhost"
     end
-  end
-
-  def intermediate_cert_path
-    '/var/lib/https-portal/intermediate.pem'
   end
 end
