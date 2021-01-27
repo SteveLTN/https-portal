91d0594744224330a0e73610ee96d9f601d20c4b
Luka Gulin <lgulin@phy.hr>
2021-01-27 13:47:23 +0100

    Closes #7

--- fs_overlay/opt/certs_manager/certs_manager.rb
+++ fs_overlay/opt/certs_manager/certs_manager.rb
@@ -18,7 +18,7 @@ class CertsManager
     system 'mkdir -p /var/run/domains.d/'
 
     generate_dummy_certificate_for_default_server
-    OpenSSL.ensure_dhparam
+    # OpenSSL.ensure_dhparam
     OpenSSL.ensure_account_key
 
     generate_ht_access(NAConfig.domains)
